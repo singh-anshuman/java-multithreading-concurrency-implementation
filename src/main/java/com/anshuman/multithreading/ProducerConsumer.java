60758fc34defae93af63c68d7f71b4b1a8dcc970
singh-anshuman <anshumansingh137@gmail.com>
2019-06-08 17:00:25 +0530

    Added code for producer consumer problem

---
+++ src/main/java/com/anshuman/multithreading/ProducerConsumer.java
@@ -0,0 +1,20 @@
+package com.anshuman.multithreading;
+
+class Machine {
+
+}
+
+class Operator {
+
+}
+
+class Design {
+    private String designDescription;
+}
+
+public class ProducerConsumer {
+
+    public static void main(String[] args) {
+
+    }
+}
